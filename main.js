b4ca315bee83b1ecb4c36e4301bdff5ae732670f
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 15:10:34 +0300

    sixth question

--- main.js
+++ main.js
@@ -29,13 +29,26 @@
 // console.log((5+6)*3)
 
 // this is the fifth exercise
-console.log((5 > 2) && false )// false
-console.log(!("knife" === "sword")) //true
-console.log((1 < 2) || (-1 > -1) || !false) // true
-console.log("") // false
-console.log((31 % 5) == "1") // true
-console.log(!!true) // true
-console.log("5th Avenue" != "5th Avenue") //false
-console.log(52 !== "52") //true
-console.log((undefined || null) )// null
+// console.log((5 > 2) && false )// false
+// console.log(!("knife" === "sword")) //true
+// console.log((1 < 2) || (-1 > -1) || !false) // true
+// console.log("") // false
+// console.log((31 % 5) == "1") // true
+// console.log(!!true) // true
+// console.log("5th Avenue" != "5th Avenue") //false
+// console.log(52 !== "52") //true
+// console.log((undefined || null) )// null
 
+// this is the sixth exercise
+let a = 3
+a = 4
+let c = 0
+let b = a
+b = 2
+a = b
+b = c
+c = a
+a = b
+console.log(a)//0
+console.log(b)//0
+console.log(c)//2
