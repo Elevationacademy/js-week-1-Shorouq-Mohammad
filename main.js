c5bf6046eb887c3c0d683c4542c90c25cc74611a
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 15:03:00 +0300

    fifth exercise

--- main.js
+++ main.js
@@ -24,6 +24,18 @@
 // }
 
 // this is the fourth exercise
-console.log(423 * 12)
-console.log(802 / 2)
-console.log((5+6)*3)
+// console.log(423 * 12)
+// console.log(802 / 2)
+// console.log((5+6)*3)
+
+// this is the fifth exercise
+console.log((5 > 2) && false )// false
+console.log(!("knife" === "sword")) //true
+console.log((1 < 2) || (-1 > -1) || !false) // true
+console.log("") // false
+console.log((31 % 5) == "1") // true
+console.log(!!true) // true
+console.log("5th Avenue" != "5th Avenue") //false
+console.log(52 !== "52") //true
+console.log((undefined || null) )// null
+
