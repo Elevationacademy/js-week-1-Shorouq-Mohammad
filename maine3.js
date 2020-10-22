3295748ecfaa22b04c4713d46c858569deb635e6
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 16:11:46 +0300

    third question

---
+++ maine3.js
@@ -0,0 +1,11 @@
+let performance = "stellar"
+let salary = 10000
+const goodBonus = 1000
+const stellarBonus = 3000
+if(performance == "stellar"){
+    salary += stellarBonus
+}else{
+    salary += goodBonus
+}
+
+console.log(salary)
