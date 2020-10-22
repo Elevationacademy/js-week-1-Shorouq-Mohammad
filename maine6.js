677e0b763811df1c7598e94ed58119e9608092df
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 16:31:04 +0300

    sixth exercise

---
+++ maine6.js
@@ -0,0 +1,11 @@
+const gender = null
+let profession = "business"
+if(gender=="female"){
+    profession += "woman"
+}else if(gender == "male"){
+    profession += "man"
+}else{
+    profession += "person"
+}
+
+console.log(profession)
