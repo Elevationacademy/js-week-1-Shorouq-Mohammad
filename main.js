1b3d9de74907e9cbade904cc9aec141e937a6946
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 14:49:47 +0300

    third exercise

--- main.js
+++ main.js
@@ -9,8 +9,16 @@
 // console.log(purposeInLife)
 
 // this is the second exercise
-const myVariable = "The potential of the future " 
-const var2 = "is the best thing ever"
-const result = myVariable + var2
+// const myVariable = "The potential of the future " 
+// const var2 = "is the best thing ever"
+// const result = myVariable + var2
+// console.log(result)
 
-console.log(result)
+// this is the third exercise
+let password = "something"
+let confirmPassword = "something"
+if (password === confirmPassword){
+    console.log("they are a match")
+}else{
+    console.log("they are not a match")
+}
