f73cd18124e9a950ee4c64f50797daa5d64580ba
Shorouq Fakhoury <Shorouq.fakhoury@gmail.com>
2020-10-22 14:45:39 +0300

    second exercise

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const maxCapacity = 14
-let surfTime = true
-let bestStudent
-const purposeInLife
-console.log(maxCapacity)
-console.log(surfTime)
-console.log(bestStudent)
-console.log(purposeInLife)
+//this is first exercise
+// const maxCapacity = 14
+// let surfTime = true
+// let bestStudent
+// const purposeInLife
+// console.log(maxCapacity)
+// console.log(surfTime)
+// console.log(bestStudent)
+// console.log(purposeInLife)
+
+// this is the second exercise
+const myVariable = "The potential of the future " 
+const var2 = "is the best thing ever"
+const result = myVariable + var2
+
+console.log(result)
